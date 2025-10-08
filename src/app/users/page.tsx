bb50e6f1a5802b6418f9eb0b72fbc775cf31fa02
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2025-10-08 16:17:12 +0400

    Fix

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,65 @@
-import UsersPage from '@/pages/users/UsersPage'
+'use client'
 
-export default function Page() {
-  return <UsersPage />
+import { Card, CardContent, CardHeader, CardTitle } from '@/shared/ui'
+import { UsersTable } from '@/widgets/users-table'
+import { UserCreateModal } from '@/features/user-create'
+import { UserEditModal } from '@/features/user-edit'
+import { config } from '@/shared/config'
+import { useUsersPage } from '@/app/users/useUsersPage'
+
+export default function UsersPage() {
+  const {
+    users,
+    loading,
+    totalUsers,
+    currentPage,
+    editingUser,
+    createModalOpen,
+    editModalOpen,
+    handlePageChange,
+    handleCreateSuccess,
+    handleEditSuccess,
+    handleEdit,
+    handleRefresh,
+    handleEditCancel,
+    createModalControls,
+  } = useUsersPage()
+
+  return (
+    <div className="container mx-auto p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Управление пользователями</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <UsersTable
+            users={users}
+            loading={loading}
+            onEdit={handleEdit}
+            onCreate={createModalControls.setTrue}
+            onRefresh={handleRefresh}
+            pagination={{
+              current: currentPage,
+              pageSize: config.pagination.defaultPageSize,
+              total: totalUsers,
+              onChange: handlePageChange,
+            }}
+          />
+        </CardContent>
+      </Card>
+
+      <UserCreateModal
+        open={createModalOpen}
+        onCancel={createModalControls.setFalse}
+        onSuccess={handleCreateSuccess}
+      />
+
+      <UserEditModal
+        open={editModalOpen}
+        user={editingUser}
+        onCancel={handleEditCancel}
+        onSuccess={handleEditSuccess}
+      />
+    </div>
+  )
 }
